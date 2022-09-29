eee6791b784bdd08eb18be3390d5a6e9466757f9
gordon-jin <jinchengguo1028@hotmail.com>
2022-09-29 15:20:48 +0900

    greeting tanzu

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello TAPTAP!\n');
+  res.send('Greeting Tanzu!\n');
 });
 
 app.listen(PORT, HOST);
