0f0a3a29e2d4b5a052417fdba635c4ffae43c7c0
Gordon Jin <jinchengguo1028@hotmail.com>
2022-11-23 15:06:50 +0900

    update with greeting TAP

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Greeting Tanzu!\n');
+  res.send('Greeting TAP!\n');
 });
 
 app.listen(PORT, HOST);
