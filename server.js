be8ae362f2a92be00fe39e5f2216b45227116824
Gordon Jin <jinchengguo1028@hotmail.com>
2022-11-28 14:08:43 +0900

    add TAP

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Greeting hello tanzu!\n');
+  res.send('Greeting hello tanzu and TAP!\n');
 });
 
 app.listen(PORT, HOST);
