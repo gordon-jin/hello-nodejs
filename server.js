c2290a40d6600a47f4b9c1de9f00167cb51b7b94
Gordon Jin <jinchengguo1028@hotmail.com>
2022-12-14 16:24:29 +0900

    edit hello world

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Greeting hello tanzu and TAP!\n');
+  res.send('hello world and tanzu\n');
 });
 
 app.listen(PORT, HOST);
